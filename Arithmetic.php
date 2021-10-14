bcdeadf52ef5109ee0360da2f822ccfed76ace0c
TheresaHongEikXuan <92510855+TheresaHongEikXuan@users.noreply.github.com>
2021-10-14 16:12:49 +0800

    First updated
    Updated of files

---
+++ Arithmetic.php
@@ -0,0 +1,16 @@
+<?php
+//arithmetic.php
+//perform arithmetic operations and assign the result to varibles
+$addition = 2+4;
+$subtraction = 6-2;
+$multiplication = 5*3;
+$division = 15/3;
+$modulus = 5%2;
+
+// output the result
+echo "perform addition: 2+4 =".$addition."<br>";
+echo "perform subtraction: 6-2 =".$subtraction."<br>";
+echo "perform multiplication: 5*3 =".$multiplication."<br>";
+echo "perform division: 15/3 =".$division."<br>";
+echo "perform modulus: 5%2 =".$modulus;
+?>
